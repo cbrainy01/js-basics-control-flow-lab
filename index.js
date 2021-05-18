ec6496af866777d172cc011f7396f4d12cbb0ccf
Carl <carlbrainegebe@gmail.com>
2021-05-18 17:31:48 -0500

    commit tree 95827b62db58e242b7fd4ba32ae68b2e336b03df

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let response;
+  if (distance >= 2000) {
+    if(distance > 2500) {
+      response = "No can do.";
+    } else {response = "I will gladly take your thirty bucks.";}
+  }
+
+  else if (distance <= 400) {
+    response = "This one is on me!";
+  }
+  
+  return response;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  const answer = destination == "NYC" ? "Ok, sounds good." : "No go."; 
+return answer;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let reply;
+  switch (tip) {
+    case "generous":
+      reply = "Thank you so much.";
+      break;
+    case "not as generous":
+     reply = "Thank you.";
+     break;
+    default: reply = "Bye.";
+      break;
+  }
+
+  return reply;
 }
+
